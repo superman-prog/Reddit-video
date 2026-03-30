865331faaace01e64a1be4659b7951f6517f968f
superman-prog <bommi1981s@gmail.com>
2026-03-30 13:56:42 +0530

    Update comments and fix FFmpeg installation command

--- render-build.sh
+++ render-build.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-exit on error
+# Exit on error
 set -o errexit
 
 pip install -r requirements.txt
 
-# Install FFmpeg
+# Install FFmpeg for Render
 mkdir -p bin
 curl -L https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-linux64-gpl.tar.xz | tar -xJ --strip-components=2 -C bin/ ffmpeg-master-latest-linux64-gpl/bin/ffmpeg

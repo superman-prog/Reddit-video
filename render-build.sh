50e71edb0337cff53acec2accafe48e1211469d0
superman-prog <bommi1981s@gmail.com>
2026-03-30 13:44:50 +0530

    Add files via upload

---
+++ render-build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+exit on error
+set -o errexit
+
+pip install -r requirements.txt
+
+# Install FFmpeg
+mkdir -p bin
+curl -L https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-linux64-gpl.tar.xz | tar -xJ --strip-components=2 -C bin/ ffmpeg-master-latest-linux64-gpl/bin/ffmpeg
